57d56b169aa28961c043b37dfe4ed792f583b480
Murren <merwou@hotmail.com>
2023-10-06 15:47:05 +0200

    Mac support fix

--- FrogeEngine/Transform.cs
+++ FrogeEngine/Transform.cs
@@ -1,9 +1,23 @@
-﻿namespace FrogeEngine;
+﻿using System.Numerics;
+
+namespace FrogeEngine;
 
 public class Transform : Component
 {
-    public Transform(GameObject parent) : base(parent)
-    {
-        
-    }
+    public Transform(GameObject parent) : base(parent) { }
+
+    // Public Properties
+    public Vector2 Position { get; set; } = Vector2.Zero;
+    public Vector2 Scale { get; set; } = Vector2.One;
+    public float Rotation { get; set; } = 0;
+    
+    // Private Properties
+    
+    
+    // Public Methods
+    
+    
+    // Private Methods
+    
+    
 }
