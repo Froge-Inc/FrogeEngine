757436b5f333ab03054477bac8608ac4409a08d2
MVargues <mccv0880@gmail.com>
2023-10-06 16:04:21 +0200

    gitignore added

--- FrogeEngine/GameObject.cs
+++ FrogeEngine/GameObject.cs
@@ -1,6 +1,13 @@
-﻿namespace FrogeEngine;
+﻿using System.Collections.Generic;
+
+namespace FrogeEngine;
 
 public class GameObject
 {
+    public Transform Transform => _components[0] as Transform; //Components[0] is reserved for Transform and will ALWAYS have a reference in it, hence no checks.
+    
+    
+    private List<Component> _components;
+    
     
 }
