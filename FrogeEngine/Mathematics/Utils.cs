b414a3edde3a6afec83b2d7a15bc3266e43332dc
Murren <merwou@hotmail.com>
2023-10-06 20:57:06 +0200

    Added Mathematics.Utils, Mathematics.Line, finished Transform

---
+++ FrogeEngine/Mathematics/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace FrogeEngine.Mathematics;
+
+public static class Utils
+{
+
+
+
+    public static Vector2 RotatedVector(Vector2 vec, float rot)
+    {
+        // Mathematical rotation of a vector by rot radians.
+        float x = vec.X * (float)Math.Cos(rot) - vec.Y * (float)Math.Sin(rot);
+        float y = vec.X * (float)Math.Sin(rot) - vec.Y * (float)Math.Cos(rot);
+        return new(x,y);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 FrogeEngine, a revolutionary MonoGame shield extending and revamping its features to provide an engine-like approach.
+
+Multi-platform support, inspired from Unity's component system, and fully open source!
+
+https://github.com/Froge-Inc/FrogeEngine
