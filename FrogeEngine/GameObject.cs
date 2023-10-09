c78fab54c6f559ba7021fb0c185599cb1ba73d5a
MVargues <mccv0880@gmail.com>
2023-10-09 12:26:30 +0200

    game object

--- FrogeEngine/GameObject.cs
+++ FrogeEngine/GameObject.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace FrogeEngine;
 
 public class GameObject
 {
+    public string Name { get => _name; }
     public Transform Transform => _components[0] as Transform; //Components[0] is reserved for Transform and will ALWAYS have a reference in it, hence no checks.
-    
-    
+
+    private string _name;
     private List<Component> _components;
     
+
+    public GameObject(string name)
+    {
+        _name = name;
+        _components = new List<Component>()
+        {
+            new Transform(null)
+        };
+    }
     
+    public T GetComponent<T>() where T: Component
+    {
+        return _components.OfType<T>().FirstOrDefault();
+    }
+
+    public T AddComponent<T>() where T: Component, new()
+    {
+        var newComponent = new T();
+        _components.Add(newComponent);
+        return newComponent;
+    }
 }
